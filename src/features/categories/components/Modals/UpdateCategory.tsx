185cf506ca8061f3121fe58b0fff4d3162800e10
Mateus Queirós <mateuscqueiros@gmail.com>
2024-06-20 09:39:04 -0300

    Parte 1B - 20/06

--- src/features/categories/components/Modals/UpdateCategory.tsx
+++ src/features/categories/components/Modals/UpdateCategory.tsx
@@ -1,6 +1,8 @@
 import { Modal } from '@/components/elements';
 import { useUser } from '@/features/auth';
 import { useModalStore } from '@/stores/modals';
+import { useRouter } from 'next/navigation';
+import { toast } from 'sonner';
 import { useCategories, useUpdateCategory } from '../../api';
 import { CategoryFormType } from '../../types';
 import { DefaultCategoryForm } from '../Form';
@@ -11,6 +13,7 @@ export function UpdateCategoryModal() {
     .modals()
     .find((m) => m.name === thisModalName);
 
+  const router = useRouter();
   const { data: user } = useUser();
   const { data: categories } = useCategories();
   const categoryData = categories.find((c) => c.id === thisModalState?.dataId)!;
@@ -24,7 +27,26 @@ export function UpdateCategoryModal() {
         ...values,
         userId: user.id,
       };
-      updateCategory.mutateAsync({ data, id: categoryData.id });
+      updateCategory
+        .mutateAsync({ data, id: categoryData.id })
+        .then(() => {
+          closeModal(thisModalName);
+          toast.success('Categoria atualizada');
+        })
+        .catch((err) => {
+          if (err.response.status === 401) {
+            toast.error('Você não está logado', {
+              action: {
+                label: 'Ir para login',
+                onClick: () => router.push('/login'),
+              },
+            });
+            return;
+          }
+          toast.error('Não foi possível criar a transação', {
+            description: err.message,
+          });
+        });
     }
   };
 
