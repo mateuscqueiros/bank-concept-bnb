fc7dffe5746147b0c5abaf069d28e0eb72e9d8d3
Mateus Queirós <mateuscqueiros@gmail.com>
2024-06-20 10:19:18 -0300

    Parte 1D - 20/06

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -2,7 +2,7 @@ import { IconLoader } from '@tabler/icons-react';
 
 export default function Loading() {
   return (
-    <div className="flex justify-center items-center w-full h-full">
+    <div className="flex justify-center items-center w-full h-screen">
       <IconLoader className="rounded-full animate-spin" />
       <span>Carregando...</span>
     </div>
---
+++ src/app/not-found.tsx
@@ -0,0 +1,7 @@
+export default function NotFoundPage() {
+  return (
+    <div className="flex justify-center items-center w-full h-screen">
+      <span className="font-bold">404</span>&nbsp;- Página não encontrada
+    </div>
+  );
+}
